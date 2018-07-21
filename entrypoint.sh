cc945bafb1d8fad9ea2e5d41cac9ca396dd254ff
wnxd <514543271@qq.com>
2018-07-22 01:20:31 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
-rm -f /etc/motd
-cat >> /etc/motd << EOF
 
-Author : wnxd <imiku@wnxd.me>
-Project : https://hub.docker.com/r/wnxd/docker-v2ray/
-Docker Image : wnxd/docker-v2ray
-
-Linux Version : $(cat /etc/redhat-release)
-Kernel Version : $(uname -r)
-Hostname : $(uname -n)
-
-Enjoy your Docker-Linux Node !
-
-EOF
-
-echo "Start Success !"
-
-tail -f /etc/v2ray/config.json
+/usr/bin/v2ray -config=/etc/v2ray/config.json
