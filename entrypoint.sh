c79c19bb4ebfc57cde67887be59b3aea5220ab9e
wnxd <514543271@qq.com>
2018-07-22 02:17:11 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+rm -f /etc/motd
+cat >> /etc/motd << EOF
+
+Author : wnxd <imiku@wnxd.me>
+Project : https://hub.docker.com/r/wnxd/docker-v2ray/
+Docker Image : wnxd/docker-v2ray
+
+Linux Version : $(cat /etc/redhat-release)
+Kernel Version : $(uname -r)
+Hostname : $(uname -n)
+
+Enjoy your Docker-Linux Node !
+
+EOF
 
 echo "Start Success !"
 
-tail -f /etc/v2ray/config.json
+sed -i "s/\[SS_PORT\]/$SS_PORT/g" /etc/v2ray/config.json
