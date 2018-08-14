6a4d4a22e24711a09bb44bd1785f6ad98f85e045
wnxd <514543271@qq.com>
2018-08-14 15:54:57 +0800

    Update relay.sh

--- Relay/relay.sh
+++ Relay/relay.sh
@@ -1,1 +1,17 @@
-echo "relay"
+tmp=${TMPDIR-/tmp}/tmp.$$
+
+cat >> $tmp << EOF
+    {
+      "port": ${RELAY_PORT},
+      "protocol": "dokodemo-door",
+      "settings": {
+        "address": "${RELAY_TARGET_ADDRESS}",
+        "port": ${RELAY_TARGET_PORT},
+        "network": "tcp,udp"
+      }
+    },
+EOF
+
+sed -i "" "20r${tmp}" /etc/v2ray/config.json
+
+rm -rf $tmp
