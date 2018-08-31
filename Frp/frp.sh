5035ab7ae372b7570cf24aa6da8ba52b12ee66ab
wnxd <imiku@wnxd.me>
2018-08-31 11:17:18 +0800

    update

--- Frp/frp.sh
+++ Frp/frp.sh
@@ -1,8 +1,7 @@
 frpc=/etc/frp/frpc.ini
 
-appendFrp()
-{
-    cat >> $frpc << EOF
+appendFrp() {
+	cat >>$frpc <<EOF
 [range:frp_port]
 type = ${1}
 local_ip = ${2}
@@ -11,17 +10,19 @@ remote_port = ${4}
 EOF
 }
 
-sed -i "s/\[FRP_SERVER_ADDRESS\]/${SS_PORT}/g" $frpc
+sed -i "s/\[FRP_SERVER_ADDRESS\]/${FRP_SERVER_ADDRESS}/g" $frpc
+sed -i "s/\[FRP_SERVER_PORT\]/${FRP_SERVER_PORT}/g" $frpc
+
 arr=${FRP_CLIENT_INFO//\|/ }
 appendFrp ${arr[@]}
 
 i=2
 extra=FRP_CLIENT_INFO_${i}
-while [ [ ! -z "${!extra}"] ]; do
-    arr=${!extra//\|/ }
-    appendFrp ${arr[@]}
-    i++
-    extra=FRP_CLIENT_INFO_${i}
+while [ ! -z "${!extra}" ]; do
+	arr=${!extra//\|/ }
+	appendFrp ${arr[@]}
+	((i++))
+	extra=FRP_CLIENT_INFO_${i}
 done
 
 cat $frpc
