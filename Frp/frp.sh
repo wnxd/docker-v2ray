8cc28567e8a98792d533188e97fa4b1fd3d8fb6d
wnxd <514543271@qq.com>
2018-08-31 09:52:35 +0800

    Create frp.sh

---
+++ Frp/frp.sh
@@ -0,0 +1,25 @@
+frpc=/etc/frp/frpc.ini
+
+appendFrp()
+{
+    cat > $frpc << EOF
+    [range:frp_port]
+    type = \${1}
+    local_ip = \${2}
+    local_port = \${3}
+    remote_port = \${4}
+    EOF
+}
+
+sed -i "s/\[FRP_SERVER_ADDRESS\]/${SS_PORT}/g" $frpc
+appendFrp (${FRP_CLIENT_INFO//|/ })
+
+i=2
+extra=FRP_CLIENT_INFO_${i}
+while [ ! -z "${!extra}"]; do
+    appendFrp (${!extra//|/ })
+    i++
+    extra=FRP_CLIENT_INFO_${i}
+done
+
+(/usr/bin/frpc -c /etc/frp/frpc.ini) &
