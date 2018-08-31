a3a7e38059861c512a8d657739a3c9d8595bfde9
wnxd <514543271@qq.com>
2018-08-31 09:20:50 +0800

    Update relay.sh

--- Relay/relay.sh
+++ Relay/relay.sh
@@ -2,11 +2,11 @@ tmp=${TMPDIR-/tmp}/relay$$
 
 cat > $tmp << EOF
     {
-      "port": ${RELAY_PORT},
+      "port": \${RELAY_PORT},
       "protocol": "dokodemo-door",
       "settings": {
-        "address": "${RELAY_TARGET_ADDRESS}",
-        "port": ${RELAY_TARGET_PORT},
+        "address": "\${RELAY_TARGET_ADDRESS}",
+        "port": \${RELAY_TARGET_PORT},
         "network": "tcp,udp"
       }
     },
